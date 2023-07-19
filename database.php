cbf736f733615c8b76900487c4a6fa131dbcf43f
Merik DeVree <c00lr4t@gmail.com>
2023-07-19 16:16:46 -0400

    started db rework, added create and display

--- database.php
+++ database.php
@@ -2,38 +2,55 @@
 // creates a database for the registered users
 class Database
 {
-    private $connection;
+    private $host = "";
+    private $db_name = "";
+    private $username = "";
+    private $password = "";
+    public $conn;
 
     public function __construct()
     {
-        $this->connection = mysqli_connect('172.31.22.43', 'Merik200462061', 'hXxnH697ga', 'Merik200462061');
-
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
         if (mysqli_connect_error()) {
-            die("Database could not connect" . mysqli_connect_error() . mysqli_connect_error());
-        }
-    }
-
-    public function create($username, $password)
-    {
-        $username = $this->sanitizer($username);
-        $password = $this->sanitizer($password);
-
-        $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-
-        $res = mysqli_query($this->connection, $sql);
-
-        if ($res) {
-            return true;
+            trigger_error("Failed to connect to MySQL: " . mysqli_connect_error());
         } else {
-            return false;
+            return $this->conn;
         }
     }
 
-    public function sanitizer($var)
+    public function insertData($post)
     {
-        $return = mysqli_real_escape_string($this->connection, $var);
-        return $return;
+        $username = $this->conn->real_escape_string($_POST['username']);
+        $email = $this->conn->real_escape_string($_POST['email']);
+        $password = $this->conn->real_escape_string($_POST['password']);
+        $query = "INSERT INTO users(username, email, password) VALUES ('$username', '$email', '$password')";
+        $sql = $this->conn->query($query);
+        if ($sql == true) {
+            //yeah idk
+            header("Location: login.php?msg1=insert");
+        } else {
+            echo "Registration failed, please try again.";
+        }
     }
+
+    public function displayData()
+    {
+        $query = "SELECT * FROM users";
+        $result = $this->conn->query($query);
+        if ($result->num_rows > 0) {
+            $data = array();
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            return $data;
+        } else {
+            echo "<p>No found records</p>";
+        }
+    }
+
+    //edit
+
+    //delete
 }
 
 $database = new Database();
