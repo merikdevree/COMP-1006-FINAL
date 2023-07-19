587b530c002416e4be723d9fcd6da89702cce9ec
Merik DeVree <c00lr4t@gmail.com>
2023-07-19 13:11:41 -0400

    users fuck

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
- 
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -16,7 +16,7 @@
             <ol>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="login.php">Login</a></li>
-                <li><a href="#">Users</a></li>
+                <li><a href="users.php">Users</a></li>
                 <li><a href="about.php">About</a></li>
             </ol>
         </nav>
@@ -27,4 +27,3 @@
 </body>
 
 </html>
-
