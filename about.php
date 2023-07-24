e65aba3c7bb47f3f9e831f036bf2fca08d61791c
GrantConnor <grentjconnor@gmail.com>
2023-07-23 20:34:08 -0400

    ^

--- about.php
+++ about.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./styles/reset.css">
     <link rel="stylesheet" href="./styles/styles.css">
+    <script src="/javascript/form.js"></script>
     <title>About</title>
 
 </head>
@@ -21,6 +22,22 @@
             </ol>
         </nav>
     </header>
+    <button class="open-button" onclick="openForm()">Login</button>
+
+    <div class="form-popup" id="myForm">
+        <form action="login.php" method="post" class="form-container">
+
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="Username" required>
+
+            <label for="password"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="Password" required>
+
+            <button type="submit" class="btn">Login</button>
+            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+        </form>
+    </div>
+
     <footer>
         <p>copyright i guess</p>
     </footer>
